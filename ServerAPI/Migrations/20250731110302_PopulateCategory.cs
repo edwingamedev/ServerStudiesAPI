685a387e74e3947411ee3aed037dd5e2bf4dde3f
Edwin Game Dev <edwinjonesholanda@gmail.com>
2025-07-31 12:13:56 +0100

    Create populate migrations

---
+++ ServerAPI/Migrations/20250731110302_PopulateCategory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFirstAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Categories (Name, ImgUrl) values ('Drink','drinks.jpg')");
+            migrationBuilder.Sql("Insert into Categories (Name, ImgUrl) values ('Food','foods.jpg')");
+            migrationBuilder.Sql("Insert into Categories (Name, ImgUrl) values ('Dessert','desserts.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categories");
+        }
+    }
+}
---
+++ ServerAPI/Migrations/20250731110638_PopulateProducts.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFirstAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "Insert into Products(Name, Description, Price, ImgUrl, Stock, CreatedAt, CategoryId) Values ('Soda Diet', 'Pop soda 350ml', 5.45, 'soda.jpg', 50, now(), 1)");
+            migrationBuilder.Sql(
+                "Insert into Products(Name, Description, Price, ImgUrl, Stock, CreatedAt, CategoryId) Values ('Tuna Sandwich', 'Tuna Sandwich with Mayo', 8.50, 'tunaSandwich.jpg', 10, now(), 2)");
+            migrationBuilder.Sql(
+                "Insert into Products(Name, Description, Price, ImgUrl, Stock, CreatedAt, CategoryId) Values ('Pudding 100 g', 'Milk Pudding 100g', 6.75, 'pudding.jpg', 20, now(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Products");
+        }
+    }
+}
